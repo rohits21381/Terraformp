a52771627a9413d878a10c3b873461881703e28b
Rohit <rohit.shinde9509@gmail.com>
2025-01-15 12:04:08 +0530

    sss

---
+++ ec2-creation-with-variables/variables.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    type = string
+    default = "ami-07b69f62c1d38b012"
+  
+}
+
+variable "type" {
+    type = string
+    default = "t2.micro"
+
+  
+}
+
+variable "keyname" {
+    type = string
+    default = "instancekey"
+  
+}
