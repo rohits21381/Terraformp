b45c930f80a9dc4f2036b7fd4a1e9a115fca7a1f
Rohit <rohit.shinde9509@gmail.com>
2025-01-15 12:29:40 +0530

    ec2-terraform-tfvars

---
+++ ec2-creation-with-terraform.tfvars/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-07b69f62c1d38b012"
+instance_type = "t2.micro"
+keyname = "instancekey"
---
+++ ec2-creation-with-terraform.tfvars/variables.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  type = string
+  default = ""
+}
+
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+variable "keyname" {
+    type = string
+    default = ""
+  
+}
